18f3db7417eb73d96befd395345061e5bb4b467d
Damola Omotosho <damola.omotosho@gmail.com>
2017-08-07 01:07:18 -0700

    Add support for iOS Page Renderer

---
+++ XamFormsNativePages/XamFormsNativePages.iOS/MainPageRenderer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamFormsNativePages;
+using XamFormsNativePages.iOS;
+
+[assembly: ExportRenderer(typeof(MainPage), typeof(MainPageRenderer))]
+namespace XamFormsNativePages.iOS
+{
+    public partial class MainPageRenderer : PageRenderer
+    {
+        private MainPage Page
+        {
+            get { return Element as MainPage; }
+        }
+
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            var oldPage = e.OldElement as MainPage;
+            if(oldPage != null)
+            {
+                oldPage.PropertyChanged -= OnPagePropertyChanged;
+            }
+
+            var newPage = e.NewElement as MainPage;
+            if (newPage != null)
+            {
+                newPage.PropertyChanged += OnPagePropertyChanged;
+            }
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            UpdateButtonText();
+        }
+
+        private void OnPagePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == MainPage.RandomNumberProperty.PropertyName)
+            {
+                UpdateButtonText();
+            }
+        }
+
+        private void UpdateButtonText()
+        {
+            if(IsViewLoaded && Page != null)
+            {
+                button.SetTitle(Page.RandomNumber.ToString(), UIControlState.Normal);
+            }
+        }
+
+        partial void ButtonPressed(UIButton sender)
+        {
+            if(Page != null)
+            {
+                Page.OnButtonPressed();
+            }
+        }
+
+        public MainPageRenderer()
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ XamFormsNativePages/XamFormsNativePages.iOS/MainPageRenderer.designer.cs
@@ -0,0 +1,32 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XamFormsNativePages.iOS
+{
+    [Register ("MainPageRenderer")]
+    partial class MainPageRenderer
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button { get; set; }
+
+        [Action ("ButtonPressed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void ButtonPressed (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (button != null) {
+                button.Dispose ();
+                button = null;
+            }
+        }
+    }
+}
