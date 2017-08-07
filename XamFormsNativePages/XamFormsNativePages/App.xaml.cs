2da54154fd5cbd0e5d154bad5309e0a92f374b33
Damola Omotosho <damola.omotosho@gmail.com>
2017-08-07 00:27:58 -0700

    Initial commit

---
+++ XamFormsNativePages/XamFormsNativePages/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamFormsNativePages
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //The root page of the application
+            MainPage = new MainPage();
+            ((MainPage)MainPage).ButtonPressed += MainPageButtonPressed;
+        }
+
+        private void MainPageButtonPressed(object sender, EventArgs e)
+        {
+            MainPage page = MainPage as MainPage;
+            page.RandomNumber = new Random().Next();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamFormsNativePages/XamFormsNativePages/MainPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamFormsNativePages
+{
+    public class MainPage:Page
+    {
+        public static readonly BindableProperty RandomNumberProperty
+            = BindableProperty.Create("RandomNumber", typeof(int), typeof(MainPage), 0);
+
+        public int RandomNumber
+        {
+            get { return (int)GetValue(RandomNumberProperty); }
+            set { SetValue(RandomNumberProperty, value); }
+        }
+
+        public event EventHandler ButtonPressed;
+
+        public void OnButtonPressed()
+        {
+            if(ButtonPressed != null)
+            {
+                ButtonPressed(this, EventArgs.Empty);
+            }
+        }
+
+    }
+}
